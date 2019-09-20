1e3289d48a53c201a32edb2a4cf0ceb61342e411
Max <max@maxs-imac.local>
2019-09-20 22:35:46 +0300

    Initial Commit

---
+++ HW2.2 Artuganova/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.2 Artuganova
+//
+//  Created by Max on 20.09.2019.
+//  Copyright © 2019 Nadzeya Artuhanava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
