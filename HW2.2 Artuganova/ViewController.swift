fdd9084ed5437ed9303f80fa6b63661ea491f4f3
Max <max@maxs-imac.local>
2019-09-21 08:30:11 +0300

    Added outlets

--- HW2.2 Artuganova/ViewController.swift
+++ HW2.2 Artuganova/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var screenView: UIView!
+    
+    @IBOutlet var radLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        screenView.layer.cornerRadius = 10
+        
+        
     }
 
 
+    @IBAction func changeRedValue() {
+    }
+    @IBAction func changeGreenValue() {
+    }
+    @IBAction func changeBlueValue() {
+    }
 }
 
