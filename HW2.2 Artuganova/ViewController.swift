08f229835c343fc02cb970d58d3ed0824aa116dc
Max <max@maxs-imac.local>
2019-09-22 17:47:13 +0300

    work with textfields

--- HW2.2 Artuganova/ViewController.swift
+++ HW2.2 Artuganova/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var screenView: UIView!
     
@@ -35,17 +35,86 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setUpView()
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     @IBAction func changeRedValue() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        redTextField.text = String(format: "%.2f", redSlider.value)
+        screenView.backgroundColor = changeScreenColor()
     }
+    
     @IBAction func changeGreenValue() {
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        greenTextField.text = String(format: "%.2f", greenSlider.value)
+        screenView.backgroundColor = changeScreenColor()
     }
+    
     @IBAction func changeBlueValue() {
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+        blueTextField.text = String(format: "%.2f", blueSlider.value)
+        screenView.backgroundColor = changeScreenColor()
     }
+    
+    private func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case redTextField:
+            redTextField.resignFirstResponder()
+        case greenTextField:
+            greenTextField.resignFirstResponder()
+        case blueTextField:
+            blueTextField.resignFirstResponder()
+        default:
+            break
+        }
+        return true
+    }
+    
+    
 }
 
 
 extension ViewController {
+
+    private func changeScreenColor() -> UIColor {
+    let screenColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        return screenColor
+    }
+    
+    private func showAlert(title: String, massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        present(alert, animated: true)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let imputValue = textField.text, !imputValue.isEmpty else { return }
+        let sliderValue = Float(imputValue)!
+        
+        if sliderValue >= 0.0 && sliderValue <= 1.0 {
+            switch textField {
+        case redTextField:
+            redSlider.value = sliderValue
+            redValue.text = imputValue
+            screenView.backgroundColor = changeScreenColor()
+        case greenTextField:
+            greenSlider.value = sliderValue
+            greenValue.text = imputValue
+            screenView.backgroundColor = changeScreenColor()
+        case blueTextField:
+            blueSlider.value = sliderValue
+            blueValue.text = imputValue
+            screenView.backgroundColor = changeScreenColor()
+        default:
+            break
+        }
+        } else {
+            showAlert(title: "Wrong format", massage: "Please, enter numbers from 0.0 to 1.0")
+        }
+    }    
     
     private func setUpView() {
         screenView.layer.cornerRadius = 10
@@ -81,5 +150,10 @@ extension ViewController {
         greenTextField.text = String(greenSlider.value)
         blueTextField.text = String(blueSlider.value)
         
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
+        
+        screenView.backgroundColor = changeScreenColor()
     }
 }
